82fe740c7a0e46d6c13bfc4d2c8cbc2e3ab2d804
dhruval1328 <sonidhruval45@gmail.com>
2024-08-13 09:34:50 +0530

    commit

--- StoreManagmentApp/src/main/java/com/cjc/main/controller/ProductController.java
+++ StoreManagmentApp/src/main/java/com/cjc/main/controller/ProductController.java
@@ -2,7 +2,18 @@ package com.cjc.main.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cjc.main.model.Product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 public class ProductController {
-
+	@GetMapping("/product")
+	public Product getpro() {
+		
+		return new Product(1,"Sagar",152.2f,"dbjkcn","mobile","Apple",true);
+	}
+	
 }
