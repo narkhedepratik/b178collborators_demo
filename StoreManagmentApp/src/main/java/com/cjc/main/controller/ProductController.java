3c279dd462325e0adb8dd79c399e0b36d0458831
narkhedepratik <pratiknarkhede039@gmail.com>
2024-08-14 09:09:20 +0530

    product put API (tonny)

--- StoreManagmentApp/src/main/java/com/cjc/main/controller/ProductController.java
+++ StoreManagmentApp/src/main/java/com/cjc/main/controller/ProductController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cjc.main.model.Product;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -14,6 +17,11 @@ public class ProductController {
 	public Product getpro() {
 		
 		return new Product(1,"Sagar",152.2f,"dbjkcn","mobile","Apple",true);
+	}
+	@PutMapping("/tonny_product/{pid}")
+	public Product updateProduct(@PathVariable int pid ,@RequestBody Product product)
+	{
+		return product;
 	}
 	
 }
